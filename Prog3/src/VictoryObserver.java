c6bf98d37d11a793366257975eecda4a37db2e0b
Francesco <92294163+Ssdj3nt@users.noreply.github.com>
2024-02-12 16:21:00 +0100

    Delete Prog3 directory

--- Prog3/src/VictoryObserver.java
+++
@@ -1,7 +0,0 @@
-class VictoryDisplay implements VictoryObserver {
-    @Override
-    public void notifyVictory() {
-        System.out.println("Hai vinto!");
-        // Qui puoi implementare la logica per visualizzare il messaggio di vittoria nel pannello di gioco o fare altre azioni necessarie
-    }
-}
